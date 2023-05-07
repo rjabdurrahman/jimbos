7a9029418534d6b5dbebd54d0670db7921aa97c8
rjabdurrahman <md.rahman0296@gmail.com>
2023-05-07 18:16:57 +0600

    Fixed: Slider height

--- components/TopSlider.js
+++ components/TopSlider.js
@@ -1,47 +1,51 @@
 import React from 'react'
 import Swiper from "react-native-swiper"
 import { View, Image } from 'react-native';
-import { LeftArrow, RightArrow } from './Arrow';
-import { Text } from 'native-base';
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function TopSlider() {
     return (
-        <Swiper
-            showsButtons={true}
-            prevButton={<Text>Prev</Text>}
-            nextButton={<Text>Next</Text>}
-            height={100}
-        >
-            <View>
+        <View style={{
+            height: 100
+        }}>
+            <Swiper
+                showsButtons={true}
+                prevButton={<FontAwesome5
+                    name="chevron-left"
+                    color="white"
+                    size={16}
+                />}
+                nextButton={<FontAwesome5
+                    name="chevron-right"
+                    color="white"
+                    size={16}
+                />}
+                height={100}
+                dotStyle={{ marginBottom: -20 }}
+                activeDotStyle={{ marginBottom: -20 }}
+            >
                 <Image
                     source={require("./images/Jimbo-organic-slider.jpg")}
-                    // resizeMode="contain"
                     style={{
                         height: 100,
                         width: '100%'
                     }}
                 />
-            </View>
-            <View>
                 <Image
                     source={require("./images/Jimbo-story-slider.jpg")}
-                    // resizeMode="contain"
                     style={{
                         height: 100,
                         width: '100%'
                     }}
                 />
-            </View>
-            <View>
                 <Image
                     source={require("./images/plastic-free.jpg")}
-                    // resizeMode="contain"
                     style={{
                         height: 100,
                         width: '100%'
                     }}
                 />
-            </View>
-        </Swiper>
+            </Swiper>
+        </View>
     )
 }
