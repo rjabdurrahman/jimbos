50ef3dbedd59aad1fdaf171b299b1c516b120790
rjabdurrahman <md.rahman0296@gmail.com>
2023-05-07 16:32:59 +0600

    Added: Search and solical icons

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Center, Flex, HamburgerIcon, Menu, Spacer } from "native-base"
 import { Pressable } from 'react-native';
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function Header() {
     return (
@@ -9,14 +10,25 @@ export default function Header() {
             py="12"
             px="4"
             bg="tertiary.800"
+            justifyItems="center"
         >
-            <Center
-                _text={{
-                    color: "white"
-                }}
-            >A</Center>
+            <Center>
+                <FontAwesome5 name="search" size={18} color="white" />
+            </Center>
             <Spacer />
-            <Center>B</Center>
+            <Center>
+                <Flex
+                    direction="row"
+                >
+                    <FontAwesome5 name="facebook" size={18} color="white" />
+                    <Spacer mx={1} />
+                    <FontAwesome5 name="twitter" size={18} color="white" />
+                    <Spacer mx={1} />
+                    <FontAwesome5 name="youtube" size={18} color="white" />
+                    <Spacer mx={1} />
+                    <FontAwesome5 name="instagram" size={18} color="white" />
+                </Flex>
+            </Center>
             <Spacer />
             <Center>
                 <Menu
