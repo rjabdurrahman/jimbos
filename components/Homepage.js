6dd045f5494ce1431288b0e1b1881879530fc0eb
rjabdurrahman <md.rahman0296@gmail.com>
2023-05-07 19:33:21 +0600

    Added: Process and scroll view

--- components/Homepage.js
+++ components/Homepage.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { View } from 'react-native';
 import TopSlider from './TopSlider';
 import Header from './Header';
+import Process from './Process';
+import { Flex, ScrollView } from 'native-base';
 
 export default function Homepage() {
     return (
@@ -10,7 +12,24 @@ export default function Homepage() {
             style={{ flex: 1 }}
         >
             <Header />
-            <TopSlider />
+            <ScrollView>
+                <View style={{ height: 4, backgroundColor: 'gray' }} />
+                <TopSlider />
+                <View style={{ height: 4, backgroundColor: 'gray' }} />
+                <Process
+                    img={require("./images/Jimbos_onsale_squares.jpg")}
+                    text="What's On Sale"
+                />
+                <Process
+                    img={require("./images/Jimbos_curbside_squares.jpg")}
+                    text="Online Ordering"
+                />
+                <Process
+                    img={require("./images/Jimbos_ingredients_sq.jpg")}
+                    text="Become a Jimbo's Vendor"
+                />
+                <View style={{ height: 4 }} />
+            </ScrollView>
         </View>
     )
 }
---
+++ components/Process.js
@@ -0,0 +1,65 @@
+import { Box, Center, Flex, Heading, Spacer, Text } from 'native-base'
+import React from 'react'
+import { Image, View } from 'react-native'
+import { useFonts, BebasNeue_400Regular } from "@expo-google-fonts/bebas-neue";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+export default function Process({
+    img,
+    text
+}) {
+    const [fontsLoaded] = useFonts({
+        BebasNeue_400Regular,
+    });
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View>
+            <Image
+                source={img}
+                alt="Jimbos"
+                resizeMode="contain"
+                style={{
+                    height: 280,
+                    width: '100%'
+                }}
+            />
+            <Box
+                bg="tertiary.100"
+                paddingY="4"
+            >
+                <Center>
+                    <Text
+                        color="tertiary.800"
+                        style={{
+                            fontFamily: "BebasNeue_400Regular",
+                            fontSize: 32,
+                            lineHeight: 32
+                        }}
+                    >
+                        {text}
+                    </Text>
+                    <Flex
+                        direction="row"
+                        justifyItems="center"
+                    >
+                        <Text
+                            color="tertiary.700"
+                            style={{ marginRight: 4, fontSize: 14 }}
+                        >
+                            Learn More
+                        </Text>
+                        <FontAwesome5
+                            name="chevron-right"
+                            color="#337D58"
+                            size={14}
+                            marginTop={3}
+                        />
+                    </Flex>
+                </Center>
+            </Box>
+        </View>
+    )
+}
